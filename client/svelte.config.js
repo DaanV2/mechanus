6eae968fac7804db142c3be7931b321beae00a59
DaanV2 <daanverstraten@hotmail.com>
2024-10-13 19:01:01 +0200

    move server render side

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -16,9 +16,10 @@ const config = {
       // these options are set automatically — see below
       pages: 'build',
       assets: 'build',
-      fallback: undefined,
+      fallback: "index.html",
       precompress: false,
-      strict: true
+      strict: true,
+      trailingSlash: true,
     })
   }
 };
