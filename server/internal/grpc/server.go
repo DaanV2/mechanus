59e30633cf82273cbcddd392e894416a4e29517a
DaanV2 <daanverstraten@hotmail.com>
2024-10-14 21:41:26 +0200

    add another error check

--- server/internal/grpc/server.go
+++ server/internal/grpc/server.go
@@ -3,6 +3,7 @@ package grpc
 import (
 	"net/http"
 
+	"github.com/charmbracelet/log"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 )
@@ -14,9 +15,12 @@ type Server struct {}
 func NewServer() {
 	mux := http.NewServeMux()
 
-	http.ListenAndServe(
+	err := http.ListenAndServe(
 		"localhost:8080",
 		// Use h2c so we can serve HTTP/2 without TLS.
 		h2c.NewHandler(mux, &http2.Server{}),
 	)
+	if err != nil {
+		log.Error("error while serving traffic", "error", err)
+	}
 }
