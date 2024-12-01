51b0cbed638b63477737d16be4f24f0d99c5cba1
DaanV2 <daanverstraten@hotmail.com>
2024-12-01 16:16:21 +0100

    extending the slice

--- server/pkg/extensions/sync/slice.go
+++ server/pkg/extensions/sync/slice.go
@@ -25,11 +25,14 @@ func (s *Slice[T]) Append(items ...T) *Slice[T] {
 	return s
 }
 
-func (s *Slice[T]) Remove(predicate func(other T) bool) {
+func (s *Slice[T]) Remove(predicate func(other T) bool) bool {
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
+	old := len(s.items)
 	s.items = slices.DeleteFunc(s.items, predicate)
+
+	return len(s.items) != old
 }
 
 func (s *Slice[T]) Items() (items []T, unlock func()) {
@@ -40,4 +43,25 @@ func (s *Slice[T]) Items() (items []T, unlock func()) {
 	}
 
 	return i, un
+}
+
+func (s *Slice[T]) WalkE(callfn func(item T) error) error {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	for _, d := range s.items {
+		err := callfn(d)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (s *Slice[T]) Walk(callfn func(item T)) {
+	_ = s.WalkE(func(item T) error {
+		callfn(item)
+		return nil
+	})
 }
