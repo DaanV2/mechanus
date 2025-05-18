bdc0716fb5f379167654479cd58747627f66e6c0
Daan Verstraten <daanverstraten@hotmail.com>
2025-05-18 23:02:28 +0200

    feat: adding more tests (#96)
    * adding more user service tests
    
    * adding campaign stuff

---
+++ server/pkg/extensions/slices/ids_test.go
@@ -0,0 +1,73 @@
+package xslices_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	xslices "github.com/DaanV2/mechanus/server/pkg/extensions/slices"
+)
+
+type testItem struct {
+	id  int
+	val string
+}
+
+func (t testItem) GetID() int { return t.id }
+
+var _ = Describe("xslices Identifiable helpers", func() {
+
+	var (
+		items []testItem
+	)
+
+	BeforeEach(func() {
+		items = []testItem{
+			{id: 1, val: "a"},
+			{id: 2, val: "b"},
+			{id: 3, val: "c"},
+		}
+	})
+
+	Describe("ContainsID", func() {
+		It("returns true if ID exists", func() {
+			Expect(xslices.ContainsID(items, 2)).To(BeTrue())
+		})
+		It("returns false if ID does not exist", func() {
+			Expect(xslices.ContainsID(items, 99)).To(BeFalse())
+		})
+	})
+
+	Describe("CollectIDs", func() {
+		It("collects all IDs", func() {
+			Expect(xslices.CollectIDs(items)).To(ConsistOf(1, 2, 3))
+		})
+	})
+
+	Describe("AddIfMissing", func() {
+		It("adds only missing items", func() {
+			newItem := testItem{id: 4, val: "d"}
+			result := xslices.AddIfMissing(items, newItem)
+			Expect(result).To(ContainElement(newItem))
+			Expect(result).To(HaveLen(4))
+		})
+		It("does not add duplicates", func() {
+			dup := testItem{id: 2, val: "b"}
+			result := xslices.AddIfMissing(items, dup)
+			Expect(result).To(HaveLen(3))
+		})
+	})
+
+	Describe("Remove", func() {
+		It("removes items with matching IDs", func() {
+			toRemove := testItem{id: 2}
+			result := xslices.RemoveID(items, toRemove)
+			Expect(result).To(HaveLen(2))
+			Expect(xslices.ContainsID(result, 2)).To(BeFalse())
+		})
+		It("does not remove items with non-matching IDs", func() {
+			toRemove := testItem{id: 99}
+			result := xslices.RemoveID(items, toRemove)
+			Expect(result).To(HaveLen(3))
+		})
+	})
+})
---
+++ server/pkg/services/campaigns/service.go
@@ -0,0 +1,77 @@
+package campaign_service
+
+import (
+	"context"
+
+	"github.com/DaanV2/mechanus/server/internal/logging"
+	"github.com/DaanV2/mechanus/server/pkg/database"
+	"github.com/DaanV2/mechanus/server/pkg/database/models"
+	xslices "github.com/DaanV2/mechanus/server/pkg/extensions/slices"
+)
+
+type Service struct {
+	db     *database.DB
+	logger logging.Enriched
+}
+
+func NewService(db *database.DB) *Service {
+	return &Service{
+		db:     db,
+		logger: logging.Enriched{}.WithPrefix("campaigns"),
+	}
+}
+
+// Gets looks up the campaign by the given id, will return a [xerrors.ErrNotExist] if nothing matched
+func (s *Service) Get(ctx context.Context, campaignId string) (*models.Campaign, error) {
+	logger := s.logger.With("campaignId", campaignId).From(ctx)
+	logger.Debug("Getting campaign by id")
+
+	var campaign models.Campaign
+
+	tx := s.db.WithContext(ctx).First(&campaign, "id = ?", campaignId)
+	if tx.Error != nil {
+		return nil, tx.Error
+	}
+
+	return &campaign, nil
+}
+
+// Create makes a new entry in the database
+func (s *Service) Create(ctx context.Context, campaign *models.Campaign) error {
+	logger := s.logger.With("campaignname", campaign.Name).From(ctx)
+	logger.Debug("Creating campaign")
+
+	tx := s.db.WithContext(ctx).Create(campaign)
+
+	return tx.Error
+}
+
+// Update will take the new information in the campaign and update the database entry. Note, this does not update the password or the ID
+func (s *Service) Update(ctx context.Context, campaign *models.Campaign) error {
+	logger := s.logger.With("campaignId", campaign.ID).From(ctx)
+	logger.Debug("updating campaign")
+
+	tx := s.db.WithContext(ctx).Omit("id").Updates(campaign)
+
+	return tx.Error
+}
+
+func (s *Service) AddUsers(ctx context.Context, campaign *models.Campaign, user *models.User) error {
+	logger := s.logger.With("campaignId", campaign.ID, "userId", user.ID).From(ctx)
+	logger.Debug("add user to campaign")
+
+	campaign.Users = xslices.AddIfMissing(campaign.Users, user)
+	user.Campaigns = xslices.AddIfMissing(user.Campaigns, campaign)
+
+	return s.db.WithContext(ctx).Model(campaign).Association("Users").Append(user)
+}
+
+func (s *Service) RemoveUser(ctx context.Context, campaign *models.Campaign, user *models.User) error {
+	logger := s.logger.With("campaignId", campaign.ID, "userId", user.ID).From(ctx)
+	logger.Debug("removing user from campaign")
+
+	campaign.Users = xslices.RemoveID(campaign.Users, user)
+	user.Campaigns = xslices.RemoveID(user.Campaigns, campaign)
+
+	return s.db.WithContext(ctx).Model(campaign).Association("Users").Delete(user)
+}
