be951d1ca2d5a136979c77989d00189563876a6a
DaanV2 <daanverstraten@hotmail.com>
2025-07-22 13:00:39 +0200

    feat: adding more role tests

---
+++ server/pkg/authenication/roles/role_test.go
@@ -0,0 +1,38 @@
+package roles_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/DaanV2/mechanus/server/pkg/authenication/roles"
+)
+
+var _ = Describe("Role", func() {
+	DescribeTable("given roles should have or inherit the role",
+		func(grant, check roles.Role) {
+			Expect(grant.Inherits(check)).To(BeTrue(), "%s should inherit %s", grant, check)
+		},
+		Entry("Admin should have viewer", roles.Admin, roles.Viewer),
+		Entry("Admin should have user", roles.Admin, roles.User),
+		Entry("Admin should have operator", roles.Admin, roles.Operator),
+		Entry("Admin should have admin", roles.Admin, roles.Admin),
+		Entry("Operator should have viewer", roles.Operator, roles.Viewer),
+		Entry("Operator should have user", roles.Operator, roles.User),
+		Entry("Operator should have operator", roles.Operator, roles.Operator),
+		Entry("User should have viewer", roles.User, roles.Viewer),
+		Entry("User should have user", roles.User, roles.User),
+		Entry("Viewer should have viewer", roles.Viewer, roles.Viewer),
+	)
+
+	DescribeTable("given roles should not have or inherit the role",
+		func(grant, check roles.Role) {
+			Expect(grant.Inherits(check)).To(BeFalse(), "%s should inherit %s", grant, check)
+		},
+		Entry("Operator should not have admin", roles.Operator, roles.Admin),
+		Entry("User should not have admin", roles.User, roles.Admin),
+		Entry("User should not have operator", roles.User, roles.Operator),
+		Entry("Viewer should not have admin", roles.Viewer, roles.Admin),
+		Entry("Viewer should not have operator", roles.Viewer, roles.Operator),
+		Entry("Viewer should not have user", roles.Viewer, roles.User),
+	)
+})
--- server/pkg/authenication/roles/service.go
+++ server/pkg/authenication/roles/service.go
@@ -6,38 +6,6 @@ import (
 	"strings"
 )
 
-const (
-	Admin    Role = "admin"
-	Operator Role = "operator"
-	User     Role = "user"
-	Viewer   Role = "viewer"
-)
-
-type Role string
-
-func (r Role) String() string {
-	return string(r)
-}
-
-func (r Role) Inherits(other Role) bool {
-	return r.value() >= other.value()
-}
-
-func (r Role) value() int {
-	switch r {
-	case Admin:
-		return 3
-	case Operator:
-		return 2
-	case User:
-		return 1
-	case Viewer:
-		return 0
-	}
-
-	return -1
-}
-
 type RoleContainer interface {
 	GetRoles() []string
 	SetRoles(roles ...string)
