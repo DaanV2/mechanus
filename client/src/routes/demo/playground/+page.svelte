27e8079d3fc5b0c9a5d0c73b1e9b30391c11710d
DaanV2 <daanverstraten@hotmail.com>
2025-08-19 19:59:18 +0200

    feat: adding playground

---
+++ client/src/routes/demo/playground/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { Application } from '$lib/2d/application';
+  import { onMount } from 'svelte';
+
+  const app = new Application();
+
+  onMount(async () => {
+    await app.init();
+  });
+</script>
