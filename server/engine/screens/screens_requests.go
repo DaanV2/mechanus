691a7a5d144381a26fb3f7d95a6713e9c1c8e33f
DaanV2 <daanverstraten@hotmail.com>
2025-10-17 15:44:43 +0200

    adding initial setup requests

--- server/engine/screens/screens_requests.go
+++ server/engine/screens/screens_requests.go
@@ -3,6 +3,7 @@ package screens
 import (
 	"context"
 
+	"github.com/DaanV2/mechanus/server/pkg/extensions/ptr"
 	screensv1 "github.com/DaanV2/mechanus/server/proto/screens/v1"
 )
 
@@ -17,11 +18,22 @@ func (s *ScreenHandler) HandlePingRequest(ctx context.Context, msg *screensv1.Cl
 }
 
 func (s *ScreenHandler) HandleInitialSetupRequest(ctx context.Context, msg *screensv1.ClientMessage_InitialSetupRequest) ([]*screensv1.ServerMessage, error) {
-	resp := []*screensv1.ServerMessage{
-		&screensv1.ServerMessage{
-			Action: screensv1.ServerMessage
-		}
-	}
-
-	return resp, nil // TODO implement handler later
+	return []*screensv1.ServerMessage{
+		{
+			Action: &screensv1.ServerMessage_InitialSetup{
+				InitialSetup: &screensv1.InitialSetupResponse{},
+			},
+		},
+		{
+			// TODO add splash screen data to the splashscreen update
+			Action: &screensv1.ServerMessage_SplashScreenUpdate{
+				SplashScreenUpdate: &screensv1.SplashScreen{
+					Show:          true,
+					Title:         ptr.To("MECHANUS"),
+					Subtitle:      ptr.To(""),
+					BackgroundHex: ptr.To("#000000"),
+				},
+			},
+		},
+	}, nil
 }
