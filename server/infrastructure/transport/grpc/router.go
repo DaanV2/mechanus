b8d429b7b07f15cc996754dfcbdeb63156943479
DaanV2 <daanverstraten@hotmail.com>
2025-10-17 15:40:10 +0200

    moving proto files

--- server/infrastructure/transport/grpc/router.go
+++ server/infrastructure/transport/grpc/router.go
@@ -7,7 +7,7 @@ import (
 	"connectrpc.com/otelconnect"
 	"github.com/DaanV2/mechanus/server/infrastructure/authentication"
 	"github.com/DaanV2/mechanus/server/infrastructure/tracing"
-	"github.com/DaanV2/mechanus/server/pkg/gen/proto/users/v1/usersv1connect"
+	"github.com/DaanV2/mechanus/server/proto/users/v1/usersv1connect"
 	"github.com/charmbracelet/log"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
